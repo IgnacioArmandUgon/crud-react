be8219c6957e02b8d7811d89a17197bce91dd08d
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-23 01:55:59 -0300

    Añadidos algunos comentarios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,90 @@
-import React, {useState} from "react";
-import {v4 as uuidv4} from 'uuid';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import UserTable from "./componentes/UserTable";
 import AddUserForm from "./componentes/AddUserForm";
-
+import EditUserForm from "./componentes/EditUserForm";
 
 const App = () => {
+
+  //Se crean algunos objetos usuarios
   const usersData = [
-     { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
-     { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
-     { id: uuidv4(), name: 'Ben', username: 'benisphere' },
-  ]
+    { id: uuidv4(), name: "Tania", username: "floppydiskette" },
+    { id: uuidv4(), name: "Craig", username: "siliconeidolon" },
+    { id: uuidv4(), name: "Ben", username: "benisphere" },
+  ];
 
   const [users, setUsers] = useState(usersData)
 
   //Agregar usuario
   const addUser = (user) => {
-    console.log(user)
-    user.id = uuidv4
-    setUsers([...users, user])
-  }
+    console.log(user);
+    user.id = uuidv4();
+    setUsers([...users, user]);
+  };
 
   //Eliminar usuario
-   const deleteUser = (id) => {
-    const usersSinElEliminado = users.filter( user => user.id !== id )
-    setUsers( usersSinElEliminado )
-   }
+  const deleteUser = (id) => {
+    const usersSinElEliminado = users.filter((user) => user.id !== id);
+    setUsers(usersSinElEliminado);
+  };
 
-    return (
-      <div className="container">
+  //Editar usuario
+  const [editing, setEditing] = useState(false);
+  
+   const [currentUser, setCurrentUser] = useState({
+     id: null,
+     name: '',
+     username: '',
+   });
+
+  const updateUser = ( id, updateUser ) =>{
+    setEditing( false )
+    setUsers( users.map( user => ( user.id === id ? updateUser : user ) ) )
+  }
+
+  const editRow = (user) => {
+    setEditing(true)
+    setCurrentUser({
+       id: user.id,
+       name: user.name,
+       username: user.username,
+     })
+  }
+
+  return (
+    <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm 
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )
+        }
+
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser}/>
+          <UserTable 
+            users={users} 
+            deleteUser={deleteUser} 
+            editRow={editRow} />
         </div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default App;
--- src/componentes/AddUserForm.js
+++ src/componentes/AddUserForm.js
@@ -1,27 +1,39 @@
-import React, { useState} from 'react'
-import {useForm} from 'react-hook-form'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const AddUserForm = (props) => {
 
-  const {register, formState: {errors}, handleSubmit} = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
+  //Al hacer click añadir usuario
   const onSubmit = (data, e) => {
-    props.addUser(data)
-
-    e.target.reset()
-  }
+    props.addUser(data);
+    e.target.reset();
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Name</label>
-      <input type="text"  {...register('name', { required: "Campo requerido" })} />
-      <span className='danger'>{errors.name && errors.name.message}</span>
-      <label>Username</label> 
-      <input type="text"  {...register('username', { required: "Campo requerido" })} />
-      <span className='danger'>{errors.username && errors.username.message}</span>
+      <input
+        type="text"
+        {...register("name", { required: "Campo requerido" })}
+      />
+      <span className="danger">{errors.name && errors.name.message}</span>
+      <label>Username</label>
+      <input
+        type="text"
+        {...register("username", { required: "Campo requerido" })}
+      />
+      <span className="danger">
+        {errors.username && errors.username.message}
+      </span>
       <button>Add new user</button>
     </form>
-  )
-}
+  );
+};
 
-export default AddUserForm
+export default AddUserForm;
