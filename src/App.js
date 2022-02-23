f3837f4e07e97b2bbf4f0081ff247c822a437fbb
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-22 23:20:39 -0300

    Implementado uuidv4 para IDs unicas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import UserTable from "./componentes/UserTable";
+import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
+  const usersData = [
+    { id: uuidv4, name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4, name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+
     return (
-        <div className="container"> 
-          <h1>Crud app con Hooks</h1>
-          <div className="flex-row">
-            <div className="flex-large">
-              <h2>Add user</h2>
-            </div>
-            <div className="flex-large">
-              <h2>View user</h2>
-              <UserTable/>
-            </div>
-          </div>
+      <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
         </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
+    </div>
     );
 }
 
--- src/componentes/UserTable.js
+++ src/componentes/UserTable.js
@@ -1,27 +1,31 @@
 import React from "react";
 
-const UserTable = () => {
+const UserTable = (props) => {
     return ( 
         <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>UserName</th>
-                <th>Actions</th>
-            </tr>
-
-        </thead>
-        <tbody>
-            <tr>
-                <td>Name data</td>
-                <td>UserName data</td>
-                <td>
-                    <button className="button muted-button">Edit</button>
-                    <button className="button muted-button">Delete</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            props.users.map(user => (
+                <tr key={user.id}>
+                    <td>Name data</td>
+                    <td>Username data</td>
+                    <td>
+                        <button className="button muted-button">Edit</button>
+                        <button className="button muted-button">Delete</button>
+                    </td>
+                </tr>
+            ))
+        }
+      
+    </tbody>
+  </table>
      );
 }
  
