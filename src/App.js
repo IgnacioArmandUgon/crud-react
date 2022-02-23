3a90b2f0ea8ca223332a3acca240e8d884ab48a3
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-22 23:06:04 -0300

    Proyecto inicial creado, se crea UserTable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UserTable from "./componentes/UserTable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container"> 
+          <h1>Crud app con Hooks</h1>
+          <div className="flex-row">
+            <div className="flex-large">
+              <h2>Add user</h2>
+            </div>
+            <div className="flex-large">
+              <h2>View user</h2>
+              <UserTable/>
+            </div>
+          </div>
+        </div>
+    );
 }
 
 export default App;
